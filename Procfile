f2cd993903adf0ce14ccf36bcd12b14306de0984
Erik <erikbaquedano1990@gmail.com>
2020-04-09 18:28:12 -0600

    This is a new commit for what I originally planned to be amended

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn computadora.wsgi --log-file -
--- computadora/urls.py
+++ computadora/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 # 
 
 
+
 from django.contrib import admin
 from django.urls import include, path
 from myapp import views# se importan todas las vistas creadas por el usuario
